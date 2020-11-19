d5667c14efc913cfdf01eb109f681d9fae181a6f
TanKARASU <karasutan@gmail.com>
2020-11-19 11:46:44 +0100

    Premiere inplmémentation de l'interface Ok reste ADDBOOK SUPPR

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,22 @@ package com.company;
 
 import com.company.objects.UserInterface;
 
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-        UserInterface userInterface = new UserInterface();
-        userInterface.launchApplication();
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    UserInterface myApp = new UserInterface();
+                    myApp.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.out.println("L'application à détecter une erreur " +
+                            "de type " + e.getMessage());
+                }
+            }
+        });
     }
 }
--- src/com/company/objects/UserInterface.java
+++ src/com/company/objects/UserInterface.java
@@ -1,122 +1,323 @@
 package com.company.objects;
 
 import com.company.BookTypeEnum;
+import com.sun.security.auth.module.JndiLoginModule;
 
+import javax.swing.*;
+import javax.swing.text.JTextComponent;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class UserInterface {
+public class UserInterface extends JFrame {
     // -------------------------------------------------
     // variables membres
     // -------------------------------------------------
     static Scanner userInput = new Scanner(System.in);
     Library myLibrary = new Library();
     ArrayList<Book> bookList = myLibrary.getBookListArray();
+    // to delete
+    String[] petString = {"", "Bird", "Cat", "Dog", "Rabbit", "Pig"};
 
-    // ------------------------------------------------- 
+    // -------------------------------------------------
+    //constructor
+    // -------------------------------------------------
+
+    public UserInterface() throws HeadlessException {
+        // création de la fenêtre principale
+        super("Bibliot-APP");
+
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setSize(1080, 760);
+
+        // création du container qui va contenir tous les éléments
+        JPanel container = (JPanel) this.getContentPane();
+        container.setLayout(new FlowLayout());
+
+        // container du formulaire
+        JPanel formContainer = (JPanel) this.getContentPane();
+        formContainer.setLayout(new FlowLayout());
+        formContainer.setVisible(true);
+
+        // Boutons Radio
+        ButtonGroup buttonGroup = new ButtonGroup();
+        JRadioButton livreRadio = new JRadioButton("Livre");
+        livreRadio.setVisible(false);
+        JRadioButton magazineRadio = new JRadioButton("MAgazine");
+        magazineRadio.setVisible(false);
+        JRadioButton mangaRadio = new JRadioButton("Manga");
+        mangaRadio.setVisible(false);
+        buttonGroup.add(livreRadio);
+        buttonGroup.add(magazineRadio);
+        buttonGroup.add(mangaRadio);
+
+        // container du texte
+        JPanel textContainer = (JPanel) this.getContentPane();
+        textContainer.setLayout(new FlowLayout());
+        textContainer.setVisible(true);
+
+        // initialisation de la barre de menu horizontale TOP
+        JMenuBar menu = new JMenuBar();
+        this.setJMenuBar(menu);
+
+        // création des libellés des menus
+        JButton addBook = new JButton();
+        addBook.setText("Ajouter Livre");
+        JButton allBooks = new JButton();
+        allBooks.setText("Tous les livres");
+        JButton oddBooks = new JButton();
+        oddBooks.setText("...index impaires");
+        JButton getStatistic = new JButton();
+        getStatistic.setText("Statistiques");
+        JButton deleteBook = new JButton();
+        deleteBook.setText("Supp. Livre");
+        JButton quitApp = new JButton();
+        quitApp.setText("QUITTER");
+        JLabel label = new JLabel();
+        label.setText("filtre");
+        JTextField textField = new JTextField();
+        textField.setPreferredSize(new Dimension(60, 25));
+
+        // to delete
+        JComboBox petList = new JComboBox(petString);
+        petList.setSelectedIndex(4);
+        petList.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(petList.getSelectedItem());
+            }
+        });
+        // to delete
+        // petList.addActionListener();
+
+        // implémentation du menu Horizontale TOP
+        menu.add(addBook);
+        menu.add(allBooks);
+        menu.add(oddBooks);
+        menu.add(getStatistic);
+        menu.add(label);
+        menu.add(textField);
+        menu.add(petList);
+        menu.add(deleteBook);
+        menu.add(quitApp);
+
+        // label du formulaire
+        JLabel titleLabel = new JLabel();
+        titleLabel.setVisible(false);
+        titleLabel.setText("Titre");
+        JLabel authorLabel = new JLabel();
+        authorLabel.setVisible(false);
+        authorLabel.setText("Author");
+        JLabel indexLabel = new JLabel();
+        indexLabel.setVisible(false);
+        indexLabel.setText("Index");
+        JLabel yearLabel = new JLabel();
+        yearLabel.setVisible(false);
+        yearLabel.setText("Année");
+        JLabel editionLabel = new JLabel();
+        editionLabel.setVisible(false);
+        editionLabel.setText("Edition");
+        JLabel languageLabel = new JLabel();
+        languageLabel.setVisible(false);
+        languageLabel.setText("Langue");
+
+        // TextField du formulaire
+        JTextField titleTextField = new JTextField();
+        titleTextField.setVisible(false);
+        titleTextField.setPreferredSize(new Dimension(60, 25));
+        JTextField authorTextField = new JTextField();
+        authorTextField.setVisible(false);
+        authorTextField.setPreferredSize(new Dimension(60, 25));
+        JTextField indexTextField = new JTextField();
+        indexTextField.setVisible(false);
+        indexTextField.setPreferredSize(new Dimension(60, 25));
+        JTextField yearTextField = new JTextField();
+        yearTextField.setVisible(false);
+        yearTextField.setPreferredSize(new Dimension(60, 25));
+        JTextField editionTextField = new JTextField();
+        editionTextField.setVisible(false);
+        editionTextField.setPreferredSize(new Dimension(60, 25));
+        JTextField languageTextField = new JTextField();
+        languageTextField.setVisible(false);
+        languageTextField.setPreferredSize(new Dimension(60, 25));
+
+        JButton submit = new JButton();
+        submit.setVisible(false);
+        submit.setText("Valider");
+
+        // affichage de tous les livres
+        JTextPane textPane = new JTextPane();
+        textPane.setText("Tous les livres\n\n" + askAllBooks());
+
+        // implémentation du formulaire d'ajout
+        formContainer.add(titleLabel);
+        formContainer.add(titleTextField);
+        formContainer.add(authorLabel);
+        formContainer.add(authorTextField);
+        formContainer.add(indexLabel);
+        formContainer.add(indexTextField);
+        formContainer.add(yearLabel);
+        formContainer.add(yearTextField);
+        formContainer.add(editionLabel);
+        formContainer.add(editionTextField);
+        formContainer.add(languageLabel);
+        formContainer.add(languageTextField);
+        formContainer.add(livreRadio);
+        formContainer.add(magazineRadio);
+        formContainer.add(mangaRadio);
+        formContainer.add(submit);
+
+        // implémentation du container texte
+        textContainer.add(textPane);
+
+        submit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String bookTitle = titleTextField.getText();
+                String bookAuthor = authorTextField.getText();
+                String bookIndex = indexTextField.getText();
+                String bookYear = yearTextField.getText();
+                String bookEdition = editionTextField.getText();
+                String bookLanguage = languageTextField.getText();
+
+                for (Book book:bookList){
+                    System.out.println(book.getBookTitle());
+                }
+
+            }
+        });
+
+        // toggle du menu ajouter un livre
+        addBook.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                toggleAddBook(titleLabel, titleTextField, authorLabel, authorTextField, indexLabel, indexTextField, yearLabel, yearTextField, editionLabel, editionTextField, languageLabel, languageTextField, livreRadio, magazineRadio, mangaRadio, submit);
+
+            }
+        });
+
+        // voir tous les livres
+        allBooks.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                hideAddBook(titleLabel, titleTextField, authorLabel,
+                        authorTextField, indexLabel, indexTextField, yearLabel,
+                        yearTextField, editionLabel, editionTextField,
+                        languageLabel, languageTextField, livreRadio,
+                        magazineRadio, mangaRadio, submit);
+                textPane.setText("Tous les livres\n\n" + askAllBooks());
+            }
+        });
+
+        // voir les livres index impairs
+
+        oddBooks.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                hideAddBook(titleLabel, titleTextField, authorLabel,
+                        authorTextField, indexLabel, indexTextField, yearLabel,
+                        yearTextField, editionLabel, editionTextField,
+                        languageLabel, languageTextField, livreRadio,
+                        magazineRadio, mangaRadio, submit);
+                textPane.setText("Tous les livres avec un index impaire\n\n" + askOddIndexedBook());
+            }
+        });
+
+        getStatistic.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                hideAddBook(titleLabel, titleTextField, authorLabel,
+                        authorTextField, indexLabel, indexTextField, yearLabel,
+                        yearTextField, editionLabel, editionTextField,
+                        languageLabel, languageTextField, livreRadio,
+                        magazineRadio, mangaRadio, submit);
+                textPane.setText("Statistiques selon le type\n\n" + getStatistic());
+            }
+        });
+
+        quitApp.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+    }
+
+    // -------------------------------------------------
     // méthodes
     // -------------------------------------------------
-    public void launchApplication() {
-        System.out.println("Welcome on this Library application");
-        System.out.println("-----------------------------------");
-        chooseOption();
+
+    private void toggleAddBook(JLabel titleLabel, JTextField titleTextField, JLabel authorLabel, JTextField authorTextField, JLabel indexLabel, JTextField indexTextField, JLabel yearLabel, JTextField yearTextField, JLabel editionLabel, JTextField editionTextField, JLabel languageLabel, JTextField languageTextField, JRadioButton livreRadio, JRadioButton magazineRadio, JRadioButton mangaRadio, JButton submit) {
+        titleLabel.setVisible(!titleLabel.isVisible());
+        titleTextField.setVisible(!titleTextField.isVisible());
+        authorLabel.setVisible(!authorLabel.isVisible());
+        authorTextField.setVisible(!authorTextField.isVisible());
+        indexLabel.setVisible(!indexLabel.isVisible());
+        indexTextField.setVisible(!indexTextField.isVisible());
+        yearLabel.setVisible(!yearLabel.isVisible());
+        yearTextField.setVisible(!yearTextField.isVisible());
+        editionLabel.setVisible(!editionLabel.isVisible());
+        editionTextField.setVisible(!editionTextField.isVisible());
+        languageLabel.setVisible(!languageLabel.isVisible());
+        languageTextField.setVisible(!languageTextField.isVisible());
+        livreRadio.setVisible(!livreRadio.isVisible());
+        magazineRadio.setVisible(!magazineRadio.isVisible());
+        mangaRadio.setVisible(!mangaRadio.isVisible());
+        submit.setVisible(!submit.isVisible());
     }
 
-    public void chooseOption() {
-        String choosenOption;
-        String regex = "^[ ]?[123456]?[ ]?$";
-        Pattern pattern = Pattern.compile(regex);
-
-        boolean result;
-        do {
-            System.out.println("Make a choice : ");
-            System.out.println("1-Add one or some books");
-            System.out.println("2-Ask for odd indexed books list");
-            System.out.println("3-Ask for books starting with specific " +
-                    "letters");
-            System.out.println("4-Ask for all available books");
-            System.out.println("5-Quit the application");
-            System.out.println("6-Statistiques");
-            choosenOption = userInput.nextLine();
-            Matcher matcher = pattern.matcher(choosenOption);
-            result = matcher.matches();
-        } while (!result);
-
-        switch (choosenOption) {
-            case "1":
-                addSomeBook();
-                break;
-            case "2":
-                askOddIndexedBook();
-                break;
-            case "3":
-                askTitleStartWithLetterA();
-                break;
-            case "4":
-                askAllBooks();
-                break;
-            case "5":
-                System.out.println("See you soon");
-                break;
-            case "6":
-                getStatistic();
-                break;
-        }
+    private void hideAddBook(JLabel titleLabel, JTextField titleTextField,
+                             JLabel authorLabel, JTextField authorTextField, JLabel indexLabel, JTextField indexTextField, JLabel yearLabel, JTextField yearTextField, JLabel editionLabel, JTextField editionTextField, JLabel languageLabel, JTextField languageTextField, JRadioButton livreRadio, JRadioButton magazineRadio, JRadioButton mangaRadio, JButton submit) {
+        titleLabel.setVisible(false);
+        titleTextField.setVisible(false);
+        authorLabel.setVisible(false);
+        authorTextField.setVisible(false);
+        indexLabel.setVisible(false);
+        indexTextField.setVisible(false);
+        yearLabel.setVisible(false);
+        yearTextField.setVisible(false);
+        editionLabel.setVisible(false);
+        editionTextField.setVisible(false);
+        languageLabel.setVisible(false);
+        languageTextField.setVisible(false);
+        livreRadio.setVisible(false);
+        magazineRadio.setVisible(false);
+        mangaRadio.setVisible(false);
+        submit.setVisible(false);
     }
 
-    public void addSomeBook() {
-        String bookType;
-        do {
-            System.out.println("What's the type of " +
-                    "Book?\n-Thriller\n-Cartoon\n-Roman");
-            bookType = userInput.nextLine();
 
-        } while (!bookType.equalsIgnoreCase("Thriller") & !bookType.equalsIgnoreCase("Roman") & !bookType.equalsIgnoreCase("Cartoon"));
 
-        System.out.println("What's the title?");
-        String bookTitle = userInput.nextLine();
-        System.out.println("Who's the Author?");
-        String bookAuthor = userInput.nextLine();
-
+/*    public void addSomeBook(String bookTitle,String bookAuthor,String bookType) {
 
         myLibrary.addBook(bookTitle, bookAuthor, bookType);
-        System.out.println(" type q for quit or anything else to add a new " +
-                "one");
-        String choice = userInput.nextLine();
 
-        if ("q".equals(choice)) {
-            System.out.println("A bientôt");
-            chooseOption();
-        } else {
-            addSomeBook();
-        }
-    }
+    }*/
 
-    public void askOddIndexedBook() {
-        System.out.println(myLibrary.getAllOddIndexedBook());
-        chooseOption();
+    public String askOddIndexedBook() {
+        return myLibrary.getAllOddIndexedBook();
     }
 
     public void askTitleStartWithLetterA() {
         System.out.println(myLibrary.getBookStartWith());
-        chooseOption();
     }
 
-    public void askAllBooks() {
-        System.out.println(myLibrary.getAllBooks());
-        chooseOption();
+    public String askAllBooks() {
+        return myLibrary.getAllBooks();
+
     }
 
-    public void getStatistic() {
-        System.out.println("Statistic according to th type of book present in" +
-                " the Library");
+    public String getStatistic() {
+        String result = "";
         for (BookTypeEnum type : BookTypeEnum.values()) {
-            System.out.println(type + ": " + rateOccurence(type.toString()) + " %");
+            result += type + ": " + rateOccurence(type.toString()) + " %\n";
         }
-        System.out.println(); // espacement
-        chooseOption();
+        return result;
     }
 
     private float rateOccurence(String query) {
@@ -131,4 +332,11 @@ public class UserInterface {
         return result / 100;
     }
 
+    // -------------------------------------------------
+    // getters
+    // -------------------------------------------------
+
+    // -------------------------------------------------
+    // setters
+    // -------------------------------------------------
 }
